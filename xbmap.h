958dc5a6f56d0a9f6983eead25fb2254c2266a80
xavi-b <xavi-b@users.noreply.github.com>
2020-04-30 17:26:18 +0200

    xbmap.h

---
+++ xbmap.h
@@ -0,0 +1,23 @@
+#ifndef XBMAP_H
+#define XBMAP_H
+
+#include <map>
+
+namespace XB
+{
+
+template<typename Key, typename Data>
+bool containsKey(std::map<Key, Data> const& map, Key const& key)
+{
+    return map.find(key) != map.end();
+}
+
+template<typename Key, typename Data, typename... Args>
+bool containsKey(std::map<Key, Data> const& map, Key const& key, Args const&... args)
+{
+    return containsKey(map, key) && containsKey(map, args...);
+}
+
+}
+
+#endif // XBMAP_H
